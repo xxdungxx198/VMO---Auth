1f3a27d3cb32befa66059b2d73f912735ee1e555
xxdungxx198 <68151283+xxdungxx198@users.noreply.github.com>
2021-07-14 09:19:16 +0700

    vmo

---
+++ pages/resetPassword/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ResetPassword from "../../modules/authentication/resetPassword";
+
+function Index() {
+  return <div><ResetPassword/></div>;
+}
+
+export default Index;
